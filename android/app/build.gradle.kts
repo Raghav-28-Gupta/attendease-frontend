f28c122ef456fb35e845552d26dac989a72153df
Raghav-28-Gupta <raghavgupta07159@gmail.com>
2025-11-09 14:04:57 +0530

    minor changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -13,6 +14,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -43,4 +45,8 @@ flutter {
     source = "../.."
 }
 
-apply(plugin = "com.google.gms.google-services")
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+    implementation("com.google.firebase:firebase-messaging:23.4.0") 
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+}
